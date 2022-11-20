0a08f4732391389b1b2285b3bda447f09a83ef0e
SunilPdl <mrsunilpaudel@gmail.com>
2022-11-20 18:29:05 +0545

    switch case in dart

---
+++ Day02/Switch.dart
@@ -0,0 +1,17 @@
+void main() {
+  var age = 19;
+
+  switch (age) {
+    case 1:
+      print('Bye');
+      break;
+
+    case 19:
+      print('Hi');
+      break;
+
+    default:
+      print('Invalid Input');
+      break;
+  }
+}
